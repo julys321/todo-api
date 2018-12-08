1e07dc60e80ad4875c2b4000b695be7856a98f0f
Julius Lungys <juliuslungys@gmail.com>
2018-12-08 18:19:09 +0200

    add TodoItem class

---
+++ src/main/java/lt/voidpumpkin/homework/todoapi/TodoItem.java
@@ -0,0 +1,42 @@
+package lt.voidpumpkin.homework.todoapi;
+
+public class TodoItem {
+    private String id;
+
+    private String text;
+
+    private String date;
+
+    public TodoItem() {
+    }
+
+    public TodoItem(String id, String text, String date) {
+        this.id = id;
+        this.text = text;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
