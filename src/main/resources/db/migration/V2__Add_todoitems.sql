b2881a0a8db4b1863de2aed56fd92ffb13f9e5b3
Julius Lungys <juliuslungys@gmail.com>
2018-12-09 11:54:28 +0200

    feat add mock todo items on migration

---
+++ src/main/resources/db/migration/V2__Add_todoitems.sql
@@ -0,0 +1,3 @@
+INSERT INTO todoitem (text, creationdate) VALUES ('I need an axe',to_timestamp('24-07-2018 13:00:02', 'dd-mm-yyyy hh24:mi:ss'));
+INSERT INTO todoitem (text, creationdate) VALUES ('Buy a pillow',to_timestamp('12-01-2011 18:55:37', 'dd-mm-yyyy hh24:mi:ss'));
+INSERT INTO todoitem (text, creationdate) VALUES ('Fly away',to_timestamp('10-08-2017 15:36:38', 'dd-mm-yyyy hh24:mi:ss'));
