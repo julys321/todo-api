74ba6aa0431ee2c57578110b1b8f92c52b082c73
Julius Lungys <juliuslungys@gmail.com>
2018-12-11 23:35:02 +0200

    refactor move all implementation to Service

--- src/main/java/lt/voidpumpkin/homework/todoapi/TodoListItemController.java
+++ src/main/java/lt/voidpumpkin/homework/todoapi/TodoListItemController.java
@@ -1,54 +1,34 @@
 package lt.voidpumpkin.homework.todoapi;
 
-import lt.voidpumpkin.homework.generated.db.tables.Todoitem;
-import lt.voidpumpkin.homework.generated.db.tables.records.TodoitemRecord;
-import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.List;
 
 @RestController
 public class TodoListItemController {
 
     @Autowired
-    private DSLContext dsl;
+    TodoListItemService todoListItemService;
 
-    private Todoitem todoItemTable = Todoitem.TODOITEM;
-
-    private List<TodoListItemResponse> getTodoListItemsFromDatabase() {
-        TodoitemRecord[] todoitemRecords = dsl
-                .selectFrom(todoItemTable)
-                .fetchArray();
-        return TodoListItemService.translateRecordsToResponses(todoitemRecords);
-    }
 
     @CrossOrigin
     @RequestMapping(value = "/todoList", method = RequestMethod.GET)
     public List<TodoListItemResponse> getTodoList() {
-        return getTodoListItemsFromDatabase();
+        return todoListItemService.fetchTodoListItemsFromDatabase();
     }
 
     @CrossOrigin
     @RequestMapping(value = "/todoListItem", method = RequestMethod.POST)
     //TODO make it return only the item that was added
     public List<TodoListItemResponse> addNewTodoItem(@RequestBody TodoListItemResponse todoListItemResponse) {
-        dsl.insertInto(todoItemTable)
-                .set(todoItemTable.TEXT, todoListItemResponse.getText())
-                .set(todoItemTable.CREATIONDATE, new Timestamp(Instant.now().toEpochMilli()))
-                //TODO fix bug where isArchived is always null and delete this hard code
-                .set(todoItemTable.ISARCHIVED, false)
-                .execute();
-        return getTodoListItemsFromDatabase();
+        return todoListItemService.addNewTodoItemToDatabase(todoListItemResponse);
     }
 
     @CrossOrigin
     @RequestMapping(value = "/archiveTodoListItem/{todoListItemId}", method = RequestMethod.PUT)
     public List<TodoListItemResponse> archiveTodoItem(@PathVariable Integer todoListItemId) {
-        dsl.fetchOne(todoItemTable, todoItemTable.ID.like(String.valueOf(todoListItemId)));
-        return getTodoListItemsFromDatabase();
+        return todoListItemService.setDatabaseTodoItemAsArchived(todoListItemId);
     }
 
 }
--- src/main/java/lt/voidpumpkin/homework/todoapi/TodoListItemService.java
+++ src/main/java/lt/voidpumpkin/homework/todoapi/TodoListItemService.java
@@ -1,14 +1,50 @@
 package lt.voidpumpkin.homework.todoapi;
 
+import lt.voidpumpkin.homework.generated.db.tables.Todoitem;
 import lt.voidpumpkin.homework.generated.db.tables.records.TodoitemRecord;
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TodoListItemService {
-    static public List<TodoListItemResponse> translateRecordsToResponses(TodoitemRecord[] todoitemRecords) {
+
+    @Autowired
+    TodoListItemController todoListItemController;
+
+    @Autowired
+    private DSLContext dsl;
+
+    private Todoitem todoItemTable = Todoitem.TODOITEM;
+
+    List<TodoListItemResponse> addNewTodoItemToDatabase(TodoListItemResponse todoListItemResponse) {
+        dsl.insertInto(todoItemTable)
+                .set(todoItemTable.TEXT, todoListItemResponse.getText())
+                .set(todoItemTable.CREATIONDATE, new Timestamp(Instant.now().toEpochMilli()))
+                //TODO fix bug where isArchived is always null and delete this hard code
+                .set(todoItemTable.ISARCHIVED, false)
+                .execute();
+        return fetchTodoListItemsFromDatabase();
+    }
+
+    List<TodoListItemResponse> setDatabaseTodoItemAsArchived(Integer todoListItemId) {
+        dsl.fetchOne(todoItemTable, todoItemTable.ID.like(String.valueOf(todoListItemId)));
+        return fetchTodoListItemsFromDatabase();
+    }
+
+    List<TodoListItemResponse> fetchTodoListItemsFromDatabase() {
+        TodoitemRecord[] todoitemRecords = dsl
+                .selectFrom(todoItemTable)
+                .fetchArray();
+        return turnTodoItemRecordsToResponses(todoitemRecords);
+    }
+
+    private List<TodoListItemResponse> turnTodoItemRecordsToResponses(TodoitemRecord[] todoitemRecords) {
         List<TodoListItemResponse> todoListItemsFromDatabase = new ArrayList<>();
         for (TodoitemRecord todoitemRecord : todoitemRecords) {
             todoListItemsFromDatabase.add(new TodoListItemResponse(todoitemRecord));
