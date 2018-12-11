2fa5d8e1bfc8b1f6948eb9298f92d6ce4210a4a5
Julius Lungys <juliuslungys@gmail.com>
2018-12-11 20:13:52 +0200

    add isArchived column to todoItem table

--- src/main/resources/db/migration/V1__Create_todoitem_table.sql
+++ src/main/resources/db/migration/V1__Create_todoitem_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE todoitem (
     id SERIAL PRIMARY KEY,
     text TEXT NOT NULL,
-    creationDate TIMESTAMP NOT NULL
+    creationDate TIMESTAMP NOT NULL,
+    isArchived BOOL NOT NULL
 );
