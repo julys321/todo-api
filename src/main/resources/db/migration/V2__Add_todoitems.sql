0da26599229f1891ba805c8610c672ed4acea4ce
Julius Lungys <juliuslungys@gmail.com>
2018-12-11 20:14:32 +0200

    add isArchived value when adding data and add archived items

--- src/main/resources/db/migration/V2__Add_todoitems.sql
+++ src/main/resources/db/migration/V2__Add_todoitems.sql
@@ -1,3 +1,6 @@
-INSERT INTO todoitem (text, creationdate) VALUES ('I need an axe',to_timestamp('24-07-2018 13:00:02', 'dd-mm-yyyy hh24:mi:ss'));
-INSERT INTO todoitem (text, creationdate) VALUES ('Buy a pillow',to_timestamp('12-01-2011 18:55:37', 'dd-mm-yyyy hh24:mi:ss'));
-INSERT INTO todoitem (text, creationdate) VALUES ('Fly away',to_timestamp('10-08-2017 15:36:38', 'dd-mm-yyyy hh24:mi:ss'));
+INSERT INTO todoitem (text, creationdate) VALUES ('I need an axe',to_timestamp('24-07-2018 13:00:02', 'dd-mm-yyyy hh24:mi:ss'),false);
+INSERT INTO todoitem (text, creationdate) VALUES ('Buy a pillow',to_timestamp('12-01-2011 18:55:37', 'dd-mm-yyyy hh24:mi:ss'),false);
+INSERT INTO todoitem (text, creationdate) VALUES ('Fly away',to_timestamp('10-08-2017 15:36:38', 'dd-mm-yyyy hh24:mi:ss'),false);
+INSERT INTO todoitem (text, creationdate) VALUES ('I miss sleep',to_timestamp('24-07-2001 13:00:02', 'dd-mm-yyyy hh24:mi:ss'),true);
+INSERT INTO todoitem (text, creationdate) VALUES ('Save me',to_timestamp('12-01-2003 18:55:37', 'dd-mm-yyyy hh24:mi:ss'),true);
+INSERT INTO todoitem (text, creationdate) VALUES ('Delete forntnite in future',to_timestamp('10-08-2002 15:36:38', 'dd-mm-yyyy hh24:mi:ss'),true);
